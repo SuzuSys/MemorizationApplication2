e2f358e6a9de50b0022f43cd5e5574af50cae159
SuzuSys <sigmatics@outlook.jp>
2021-04-18 17:28:02 +0900

    test element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <div id="setting"></div>
-    <Question formula="$x$"/>
+    <div id="setting">
+      <el-button icon="el-icon-notebook-2" @click="drawer = true">Add sheet</el-button>
+    </div>
+    <el-drawer
+      title="Add Sheet"
+      :visible.sync="drawer"
+      :direction="direction">
+      <span>Hi, there!</span>
+    </el-drawer>
+    <div id="sheet">
+      <Question formula="$x$"/>
+    </div>
   </div>
 </template>
 
@@ -12,11 +22,34 @@ export default {
   name: "App",
   components: {
     Question
+  },
+  data() {
+    return {
+      drawer: false,
+      direction: 'ltr'
+    }
   }
 };
 </script>
 
 <style>
 #app {
+  margin: 0px;
+}
+#sheet {
+  width: 793px;
+  margin: auto;
+  background-color: white;
+  padding: 10mm;
+}
+#setting {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+}
+@media print {
+  #setting {
+    display: none;
+  }
 }
 </style>
